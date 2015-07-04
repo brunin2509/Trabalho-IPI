e1c7793593ef500ed93588dd7e1570f5d7804e05
Brunin2509 <brunin2509@gmail.com>
2015-07-04 18:57:17 -0300

    teste

--- trab_final.m
+++ trab_final.m
@@ -11,25 +11,3 @@ Igray = rgb2gray(I);
 Imf = medfilt2(Igray, [7 7]);
 
 figure; imshow(Imf); title('Imagem com filtro de mediana');
-
-Ibordas = edge(Imf, 'Canny');
-
-figure;
-imshow(Ibordas); title('Bordas');
-
-b = strel('square', 2);
-Idilatado = imdilate(Ibordas, b);
-
-figure;
-imshow(Idilatado); title('Bordas dilatadas');
-
-Ifilt = bwareaopen(Idilatado, 300);
-figure;
-imshow(Ifilt); title('Bordas filtradas');
-
-pause();
-close all;
-
-%comentario
-%comentario 2
-%comentario 3
