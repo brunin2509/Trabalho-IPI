073c7c8d2ed8b4a829a89ca8d863b7c3921923c5
ftfnunes <ftfnunes@hotmail.com>
2015-07-04 18:22:43 -0300

    Update trab_final.m

--- trab_final.m
+++ trab_final.m
@@ -1,11 +1,31 @@
-clear all;
 close all;
+clear all;
 clc;
 
 I = imread('jailson.jpg');
-I = rgb2gray(I);
-Imf = medfilt2(I, [7 7]);
+
 figure; imshow(I); title('Imagem original');
+
+Igray = rgb2gray(I);
+
+Imf = medfilt2(Igray, [7 7]);
+
 figure; imshow(Imf); title('Imagem com filtro de mediana');
 
-a=3;
+Ibordas = edge(Imf, 'Canny');
+
+figure;
+imshow(Ibordas); title('Bordas');
+
+b = strel('square', 2);
+Idilatado = imdilate(Ibordas, b);
+
+figure;
+imshow(Idilatado); title('Bordas dilatadas');
+
+Ifilt = bwareaopen(Idilatado, 300);
+figure;
+imshow(Ifilt); title('Bordas filtradas');
+
+pause();
+close all;
