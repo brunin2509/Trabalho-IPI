e7b80651f0aee291a3caaa9f6414df7e64ecfa41
ftfnunes <ftfnunes@hotmail.com>
2015-07-05 00:26:34 -0300

    att

--- colorBilateralFil.m
+++ colorBilateralFil.m
@@ -1,21 +1,34 @@
 function[im] = colorBilateralFil(I, w, s_d, s_c)
 
 I4 = imresize(I, 0.25);
-sIm = size(I4);
+s = size(I4);
+fil = zeros(w,w);
 
-[X,Y] = meshgrid(-w:w, -w:w);
-G = exp(-(X.^2 + Y.^2)/(2*s_d^2));
+w = 9
+s_d = 3;
 
-B = zeros(sIm);
-
-for d = 1:s(3)    
-    for i = 1:s(1)
-        for j = 1:s(2)
-                        
-        end
+for i = 1:w
+    for j = 1:w
+        fil(i, j) = exp(-((round(w/2)-i)^2 + (round(w/2)-j)^2)/(2*s_d^2));
     end
 end
 
-im = imresize(B, 4);
+w2 = floor(w/2);
 
+B = zeros(sIm);
+   
+for i = 1:s(1)
+    for j = 1:s(2)
+        iInf = max(i-w, 1);
+        iSup = min(i+w, s(1));
+        jInf = max(j-w, 1);
+        jSup = min(j+w, s(2));
+        for k = iInf:iSup,
+            for m = jInf:jSup,
+               
+            end
+        end
+        
+        
+    end
 end
