68ad96df4a80ec5e8529e4fb5e6741512a6e94d1
Brunin2509 <brunin2509@gmail.com>
2015-07-05 02:39:08 -0300

    atualizado. Bruno

--- trab_final.m
+++ trab_final.m
@@ -2,8 +2,8 @@ close all;
 clear all;
 clc;
 
-%I = imread('jailson.jpg');
-I = imread('maxresdefault.jpg');
+I = imread('jailson.jpg');
+%I = imread('maxresdefault.jpg');
 
 figure; imshow(I); title('Imagem original');
 
@@ -38,7 +38,7 @@ imshow(Ifilt); title('Bordas filtradas');
 
 Ifil_bilateral = uint8(colorBilateralFil(I, 9, 3, 5));
 
-figure; imshow(Ifil_bilateral); title('Aplicação do filtro bilateral');
+figure; imshow(Ifil_bilateral); title('Aplicacao do filtro bilateral');
 
 Imf(:,:,1) = medfilt2(Ifil_bilateral(:,:,1), [7 7]);
 Imf(:,:,2) = medfilt2(Ifil_bilateral(:,:,2), [7 7]);
@@ -48,7 +48,12 @@ figure;imshow(Imf);title('Filtro de mediana na imagem colorida');
 
 Iquant = uint8((floor(double(Imf)/16))*16);
 
-figure;imshow(Iquant);title('Imagem quantizada com 16 níveis');
+figure;imshow(Iquant);title('Imagem quantizada com 16 niveis');
+
+Iresultante(:,:,1) = Iquant(:,:,1) .* uint8(~Ifilt);
+Iresultante(:,:,2) = Iquant(:,:,2) .* uint8(~Ifilt);
+Iresultante(:,:,3) = Iquant(:,:,3) .* uint8(~Ifilt);
+figure;imshow(Iresultante);title('Imagem resultante');
 
 pause();
 close all;
